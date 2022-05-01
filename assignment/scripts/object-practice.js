b03014adaf2427a3da87ede4a5ea9ba75515dfbc
Nimo Abdi <nimoabdi318@gmail.com>
2022-05-01 00:21:13 -0500

    finished up assignment and looked over for submission

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -34,6 +34,8 @@ console.log('My full name is', fullName)
   - Console.log your last favorite food 
 */
 
+console.log('First favorite food is', me.favThreeFoods[0]);
+console.log('Last favorite food is', me.favThreeFoods[me.favThreeFoods.length -1]);
 
 
 /* 4. Change a property of an existing object.
@@ -43,6 +45,9 @@ console.log('My full name is', fullName)
   - Console.log your updated shoe count. 
 */
 
+console.log(me.shoeCount);
+me.shoeCount += 1;
+console.log(me.shoeCount);
 
 /* 5. Add a new property to an existing object.
   - Add a 'favoriteColor' to your existing object
@@ -50,3 +55,6 @@ console.log('My full name is', fullName)
   - Cool huh? It works, even though it wasn't there before
   - Console.log your object
 */
+
+me.favoriteColor = 'sage green';
+console.log('My favorite color is', me.favoriteColor);
